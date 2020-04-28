36fb1c9f0854fab2bddc19a54b338a3a4b077c09
marcos <marcosrodriguesbrugnaro@gmail.com>
2020-04-27 22:29:37 -0300

    Upload project

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react'
+import api from './services/api'
+import './styles.css'
+
+function App () {
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data)
+    })
+  }, [])
+
+  async function handleAddRepository () {
+    const response = await api.post('repositories', {
+      title: 'Desafio Node.js 02',
+      url: 'https://github.com/brugnaro/bootcamp/tree/master/desafio-02',
+      techs: ['Node.js', 'React', 'React Native'],
+      likes: 0
+    })
+
+    const repository = response.data
+
+    setRepositories([...repositories, repository])
+  }
+
+  async function handleRemoveRepository (id) {
+    const response = await api.delete(`repositories/${id}`)
+
+    if (response.status === 204) {
+      setRepositories(repositories.filter(repository => repository.id !== id))
+    }
+  }
+
+  return (
+    <div>
+      <ul data-testid='repository-list'>
+        {repositories.map(repository => (
+          <li key={repository.id}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
+      </ul>
+
+      <button onClick={handleAddRepository}>Adicionar</button>
+    </div>
+  )
+}
+
+export default App
